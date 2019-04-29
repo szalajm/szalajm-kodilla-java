e52d2cdefd9407b5a2f8b7667f706ceff3757d31
Maciej Szalaj <szalajm.neo@gmail.com>
2019-04-29 18:13:43 +0200

    StreamForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,17 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beutifier.PoemBeutifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalBeutifier;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
         PoemBeutifier poemBeutifier = new PoemBeutifier();
 
         System.out.println("Calculating expressions with lambdas");
