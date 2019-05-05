b34a42c3d990c3f0d635f9bf8cb60a9930973a7f
Maciej Szalaj <szalajm.neo@gmail.com>
2019-05-05 10:57:01 +0200

    FirstChallengeTryCatch

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
@@ -21,7 +21,7 @@ public class FileReader {
 
         } catch (IOException e) {
 
-            System.out.println("Something went wrong! ERROR:" + e);
+            throw new FileReaderException();
 
         } finally {
 
